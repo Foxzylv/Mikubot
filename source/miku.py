805deadd4afc140c2f6aebd784659fc429e9574e
Foxzy <86633618+Foxzylv@users.noreply.github.com>
2022-04-05 17:01:59 -0500

    Update miku.py

--- source/miku.py
+++ source/miku.py
@@ -1,1 +1,24 @@
+import os
 import discord
+my_secret = os.environ['Token']
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content == 'Pizza is so good':
+   await message.channel.send('Go get your pizza!')
+
+  if message.content == 'Pizza':
+   await message.channel.send('Go get your pizza!')
+
+  if message.content == 'I love pizza':
+   await message.channel.send('Go get your pizza!')
+
+  if message.content == 'Pizza is amazing':
+   await message.channel.send('Go get your pizza!')
+
+client.run(os.getenv('Token'))
