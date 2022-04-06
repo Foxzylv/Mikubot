97be931e97b513330982a571bcc1a2628051e118
Foxzy <86633618+Foxzylv@users.noreply.github.com>
2022-04-06 17:09:45 -0500

    Update miku.py

--- source/miku.py
+++ source/miku.py
@@ -9,16 +9,18 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content == 'Pizza is so good':
-   await message.channel.send('Go get your pizza!')
+  if "pizza" in message.content.lower():
+    await message.channel.send("Go get your pizza!")
 
-  if message.content == 'Pizza':
-   await message.channel.send('Go get your pizza!')
+  if "miku" in message.content.lower():
+    await message.channel.send("Hello uwu")
 
-  if message.content == 'I love pizza':
-   await message.channel.send('Go get your pizza!')
+  if "hatsune" in message.content.lower():
+    await message.channel.send("Hello (>_<)")
 
-  if message.content == 'Pizza is amazing':
-   await message.channel.send('Go get your pizza!')
+  if "!sing" in message.content.lower():
+    await message.channel.send("semai kyoushitsu no katatsumuri shitauchi ga kanaderu merodi ijimekko ijimerarekko dochira mo piiman wa nigai ne daihakken kakkoii ko mo kawaii ko mo kimi no mono dakara uso demo honne de iesu no ankēto itai itai itai no wa iya dakara sakaraenai ja nai…  	“arigatou” mo “ohayou” mo “gomen nasai” mo zenbu natsukashii kotoba ni naru hi ga kuru no jūyonsai mo yonjussai mo odorou rattattatta dareka dareka minna o tasukete iraira poisute guranpuri osanago no kawaii aironii koibito mo shinryakusha mo dochira mo niku no katamari ka daijikken sāroin mo tsubame no su mo kimi no mono dakara supai no tsūshin kiroku wa shiikuretto kowai kowai kowai no wa yada kara nemutte itai ja nai… “ureshii” mo “kanashii” mo “warufuzake” mo zenbu shirokujichū kanshi sareru hi ga kuru no Satou-san mo Suzuki-san mo utaou ranranranran itsuka itsuka namae mo wasurete nakama ni nattara happii endo? nakama hazure wa doko e ikou chimamire ni natta pasupōto sentaku no shunkan wa sugu sugu sugu soba ni “sayonara” mo “oyasumi” mo “mata ashita” mo zenbu tsunagarazu ni togireru hi ga kuru no Tanaka-san mo Takahashi-san mo waraou wahhahhahha dareka dareka dareka dareka dareka “arigatou” mo “ohayou” mo “gomen nasai” mo zenbu natsukashii kotoba ni naru hi ga kuru no inbouron mo gasukonro mo odorou rattattatta dareka dareka minna o tasukete")
+
+    
 
 client.run(os.getenv('Token'))
